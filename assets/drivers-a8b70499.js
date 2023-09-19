ff3cb3bd71d2925767197e3ea5d7a457837c3918
iKOOLCORE <112603609+KoolCore@users.noreply.github.com>
2023-09-19 14:06:20 +0800

    Update R2 content

---
+++ assets/drivers-a8b70499.js
@@ -0,0 +1,1 @@
+import{A as t,f as s,e as o,g as a}from"./vendor-a7b6b0c4.js";import"./__commonjsHelpers__-725317a4.js";const c={class:"markdown-body"},r=a("p",{class:"notice"},"不预装任何系统，不提供任何固件、镜像、教程等，硬件质量之外的服务。",-1),n=[r],l=[],u={__name:"drivers",setup(_,{expose:e}){return e({frontmatter:{meta:[]}}),t({meta:[]}),(i,p)=>(o(),s("div",c,n))}};export{u as default,l as meta};
